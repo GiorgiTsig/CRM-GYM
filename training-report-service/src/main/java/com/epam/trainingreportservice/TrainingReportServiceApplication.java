d7c64b5fcc6642dd5aebb6fa0aa34a0e9c68a13c
Giorgi Tsignadze <giorgi.tsignadze.1@btu.edu.ge>
2026-04-15 22:20:46 +0400

    add eureka-server

---
+++ training-report-service/src/main/java/com/epam/trainingreportservice/TrainingReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.trainingreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingReportServiceApplication.class, args);
+    }
+
+}
---
+++ training-report-service/src/test/java/com/epam/trainingreportservice/TrainingReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.trainingreportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingReportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
