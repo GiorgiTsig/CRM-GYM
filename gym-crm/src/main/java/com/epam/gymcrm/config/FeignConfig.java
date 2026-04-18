3c8576e7ef82dd6f56a38aaf31cee34795accbd1
Giorgi Tsignadze <giorgi.tsignadze.1@btu.edu.ge>
2026-04-18 21:51:09 +0400

    implemented OAuth2AuthorizedClientManager and OAuth2AuthorizedClientProvider

--- gym-crm/src/main/java/com/epam/gymcrm/config/FeignConfig.java
+++ gym-crm/src/main/java/com/epam/gymcrm/config/FeignConfig.java
@@ -1,24 +1,37 @@
 package com.epam.gymcrm.config;
 
-import com.epam.gymcrm.service.TokenService;
 import feign.RequestInterceptor;
 import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
 
+@Configuration
 public class FeignConfig {
 
-    private final TokenService tokenService;
+    private final OAuth2AuthorizedClientManager manager;
 
-    public FeignConfig(TokenService tokenService) {
-        this.tokenService = tokenService;
+    public FeignConfig(OAuth2AuthorizedClientManager manager) {
+        this.manager = manager;
     }
 
     @Bean
     public RequestInterceptor requestInterceptor() {
         return template -> {
-            String token = tokenService.getToken();
             String correlationId = MDC.get("correlationId");
-            template.header("X-Correlation-Id", correlationId);
+
+            OAuth2AuthorizeRequest request =
+                    OAuth2AuthorizeRequest.withClientRegistrationId("my-client")
+                            .principal("feign-client")
+                            .build();
+
+            OAuth2AuthorizedClient client = manager.authorize(request);
+            String token = client.getAccessToken().getTokenValue();
+
+            if (correlationId != null) {
+                template.header("X-Correlation-Id", correlationId);
+            }
+
             template.header("Authorization", "Bearer " + token);
         };
     }
